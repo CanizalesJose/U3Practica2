2d2c6557a9e07e2fbff47d66835039d7d4f596aa
CanizalesJose <jose1605m@gmail.com>
2023-10-03 21:55:22 -0700

    v0.95 Variantes del código creadas

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const formulario = document.getElementById("formulario");
     const mensajeError = document.getElementById("mensajeError");
- 
 
     formulario.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -15,52 +14,50 @@ document.addEventListener("DOMContentLoaded", function() {
         const correo = formulario.correo.value;
         const edad = formulario.edad.value;
         const fechaNacimiento = formulario.fechaNacimiento.value;
- 
+        var mensaje = "";
 
         // Validar ID (5 dígitos exactos)
         if (!/^\d{5}$/.test(id)) {
-            mensajeError.textContent = "El ID debe tener 5 dígitos exactos.";
-            return;
+            mensaje = mensaje + "El ID debe tener 5 dígitos exactos. ";
         }
  
 
         // Validar nombre y apellidos (no pueden estar vacíos)
         if (nombre.trim() === "" || apellidos.trim() === "") {
-            mensajeError.textContent = "El nombre y los apellidos no pueden estar vacíos.";
-            return;
+            mensaje = mensaje + "El nombre y los apellidos no pueden estar vacíos. ";
         }
  
 
         // Validar teléfono (###)###-####
         if (!/^\(\d{3}\)\d{3}-\d{4}$/.test(telefono)) {
-            mensajeError.textContent = "El teléfono debe tener el formato (###)###-####.";
-            return;
+            mensaje = mensaje + "El teléfono debe tener el formato (###)###-####. ";
         }
  
 
         // Validar correo electrónico
         if (!/^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(correo)) {
-            mensajeError.textContent = "El correo electrónico no es válido.";
-            return;
+            mensaje = mensaje + "El correo electrónico no es válido. ";
         }
  
 
         // Validar edad (número positivo)
         const edadNum = parseInt(edad);
         if (isNaN(edadNum) || edadNum <= 0) {
-            mensajeError.textContent = "La edad debe ser un número positivo.";
-            return;
+            mensaje = mensaje + "La edad debe ser un número positivo. ";
         }
  
 
         // Validar fecha de nacimiento (AAAA-MM-DD)
         if (!/^\d{4}-\d{2}-\d{2}$/.test(fechaNacimiento)) {
-            mensajeError.textContent = "La fecha de nacimiento debe tener el formato AAAA-MM-DD.";
-            return;
+            mensaje = mensaje + "La fecha de nacimiento debe tener el formato AAAA-MM-DD. ";
+        }else {
+
+            mensaje = "Formulario enviado con éxito.";
         }
+
+        mensajeError.textContent = mensaje;
  
 
         // Si todas las validaciones pasaron, puedes enviar el formulario o realizar otras acciones aquí
-        mensajeError.textContent = "Formulario enviado con éxito.";
     });
 });
